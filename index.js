648095e03cd0161de888014dcbc2e8179e176dfa
Chris <cwoolnoughchamorro@gmail.com>
2018-12-12 13:26:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+function getFirstSelector(selector) {
+    return document.querySelector(selector);
+}
+
+function nestedTarget() {
+    return document.querySelector('#nested .target');
+}
+
+let x = document.querySelector("#grand-node")
+
+function deepestChild() {
+    if (x.children[0]) {
+    x = x.children[0]
+        deepestChild() 
+    } 
+        console.log(x)
+        return x  
+}
+
+// deepestChild()
+
+
+function increaseRankBy(n) {
+    const list = document.querySelectorAll('.ranked-list li')
+        for(let i =0; i < list.length; i++) {
+            let parsed = parseInt(list[i].innerHTML)
+            list[i].innerHTML = parsed + n
+        }
+}
+
+
 
