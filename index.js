10d3452786510b09dcd029e781d89fc2f92a391a
kbberker <kbbuk90@gmail.com>
2018-12-13 13:17:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  const list = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0; i < list.length; i++) {
+    let children = list[i].children;
+
+    for (let j = 0; j < children.length; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+    }
+  }
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+}
