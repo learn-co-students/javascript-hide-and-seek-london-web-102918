462120ba45ef9135c0793ea37df1afe2c1014579
paolov1928 <ventura.paolo@gmail.com>
2018-12-16 14:17:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 
+const getFirstSelector = selector => {
+  return document.querySelector(selector)
+}
+
+const nestedTarget = () => {
+  return document.querySelector('div div div div div')
+}
+// function nestedTarget (){
+//
+// }
+const deepestChild = () => {
+  return document.querySelector('#grand-node').firstElementChild.firstElementChild.firstElementChild.firstElementChild
+}
+
+// MY 1000% BETTER SOLUTION
+// const increaseRankBy = (n) => {
+//   // numberOfChildElements = document.querySelectorAll('.ranked-list').length
+//   //
+//   const lis = document.querySelectorAll('.ranked-list li');
+//     lis.forEach(li => li.innerHTML = parseInt(li.innerHTML) + parseInt(n));
+// }
+
+
+
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list');
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children;
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+    }
+  }
+}
